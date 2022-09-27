fc8048be8f8b13c1fdaaa74cae8f43b1cf08b48f
NICCK08 <65999730+NICCK08@users.noreply.github.com>
2022-09-27 19:31:55 +0530

    Updated css

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-first=document.querySelector('#first')
-but=document.querySelector('button')
-output=document.querySelector('#second')
-but.addEventListener('click',translate)
+var translateBtn = document.querySelector('#btn-translate');
+var textinput = document.querySelector('textarea');
+var outputarea = document.querySelector('#output');
 
-function translate(){
-    url='https://api.funtranslations.com/translate/minion.json'+"?"+"text="+first.value
-    console.log(url)
-    fetch(url)
-    .then(response=>response.json())
-    .then(data=>output.value=data.contents.translated)
-    .cat
+// Use Mock API for testing otherwise rate limit issue
+// var serviceURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+
+var serviceURL = 'https://api.funtranslations.com/translate/minion.json';
+
+var colors = {
+    errorColor: '#fecdd3',
+    white :'#FFFFFF'
+}
+
+function constructURL(textinput){
+    return serviceURL + '?text=' + textinput };
+
+function errorHandler(error){
+
+    if (error instanceof TypeError)
+    {
+        outputarea.innerText = 'Error Alert! It seems the Minions are busy finding Gru. Try again in an hour!'
+    } else {
+        outputarea.innerText = error
+    }
+    
+    outputarea.style.backgroundColor = colors.errorColor;
+};
+
+
+function dofetch(textinput){
+    fetch(constructURL(textinput))
+    .then(response => response.json())
+    .then(data => {var translatedText = data.contents.translated
+        outputarea.innerText = translatedText})
+    .catch(errorHandler)
+};
+
+function clickHandler() {
+    // Put styling back to default in case error occured in last call
+    outputarea.style.backgroundColor = colors.white;
+    outputarea.innerText = '';
+    var translationText = textinput.value;
+    if (translationText.length === 0) {
+        outputarea.innerText = 'Please enter something to translate!'
+        outputarea.style.backgroundColor = colors.errorColor;
+    } else {
+        dofetch(translationText);
+    }
+    
+};
+
+translateBtn.addEventListener("click", clickHandler)
